f2cc568c8c257f6c287272d3c63b970f582522a8
Alumno <alumno@2CI6C71D98C154F.(none)>
2016-01-29 18:22:47 -0300

    agregue cosas faltantes

--- 1/basic_types.py
+++ 1/basic_types.py
@@ -1,10 +1,12 @@
-integer_type = 
+integer_type = 22
 assert isinstance(integer_type, int)
 
 
-string_type = 
+
+string_type = "hola"
 assert isinstance(string_type, str)
 
 
-boolean_type = 
+
+boolean_type = True
 assert isinstance(boolean_type, bool)
