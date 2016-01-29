92d138a874dd8b62948e3d21c4902bbf65011c59
Alumno <alumno@2CI6C71D98C154F.(none)>
2016-01-29 18:48:27 -0300

    error solucionado

--- 2/collection_types.py
+++ 2/collection_types.py
@@ -1,10 +1,10 @@
-list_type = 
+list_type = ["hola",True,[1,2,3]]
 assert isinstance(list_type, list)
 
 
-tuple_type = 
+tuple_type = (1,2,3,4)
 assert isinstance(tuple_type, tuple)
 
 
-dict_type = 
+dict_type = {"perfume" :324,"crema":100}
 assert isinstance(dict_type, dict)
