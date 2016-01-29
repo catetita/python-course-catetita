fb4f0882ac6d9941a944859a94db0582edca404e
Alumno <alumno@2CI6C71D98C154F.(none)>
2016-01-29 20:08:38 -0300

    agregue lo faltantes y modifique

--- 3/flow_control.py
+++ 3/flow_control.py
@@ -1,22 +1,22 @@
 
-condition =
+condition = 42>2
 if condition:
     print("condition was True.")
-elif not :
+elif not condition: 
     print("condition was False.")
 
 
 age = 0
 while age < 19:
     age = age + 1
-    if  % 2:
+    if  age % 2:
         print("age is {}".format(age))
-    elif age = 17:
+    elif age < 17:
         break
 
 
 try:
-    4 / 0
+    4 / 2
 except Exception:
     print("This is executed when theres Exception on the Try block. Fix the Try code block !")
 else:
